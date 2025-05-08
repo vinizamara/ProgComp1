cc622eed366241a0d322d6e642d030423d34433b
VINICIUS MANFRIN ZAMARA <26458@unifacef.edu.br>
2025-05-07 22:07:02 -0300

    Atualização/Correção do exercício 1

--- EstruturaDeRepeticao/js/exercicio1.js
+++ EstruturaDeRepeticao/js/exercicio1.js
@@ -5,12 +5,16 @@ function exercicio1(){
     let insatisfeito = 0
 
     while(conta <= 10){
-        nota = Number(prompt(`Informe a nota de satisfação entre 0 a 10: ${conta}`))
-        if (nota >= 8){
+        nota = Number(prompt(`Participante ${conta}, informe a nota de satisfação entre 0 a 10:`))
+        if (nota >= 8 && nota <= 10){
             satisfeito ++
         }
-        else if (nota < 5){
+        else if (nota < 5 && nota >= 0){
             insatisfeito ++
+        }
+        else if (nota < 0 || nota > 10){
+            alert(`Nota inválida`)
+            continue // Volta para o início, sem incrementar conta
         }
         soma = soma + nota
         conta ++
@@ -18,7 +22,7 @@ function exercicio1(){
 
     let media = soma / 10
 
-    alert(`A média das notas foi: ${media}`)
+    alert(`A média das notas foi: ${media.toFixed(2)}`)
     alert(`O número de pessoas satisfeitas foi: ${satisfeito}`)
     alert(`O número de pessoas insatisfeitas foi: ${insatisfeito}`)
 }
