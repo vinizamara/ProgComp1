247a581aece7a5af5695fb2216b7dbbb04236e1a
Vinicius Manfrin <vinizamara2@gmail.com>
2025-05-09 10:05:15 -0300

    Finalização do Exercício 2

--- EstruturaDeRepeticao/js/exercicio2.js
+++ EstruturaDeRepeticao/js/exercicio2.js
@@ -1,3 +1,18 @@
 function exercicio2(){
-    
+    let preco = 5
+    let ingressosVendidos = 120
+    let despesas = 200
+    let lucro =(ingressosVendidos * preco) - despesas
+    let contagem = 0
+
+    while (preco > 1){
+        alert("Quando o preço do ingresso é: R$" + preco.toFixed(2) + "\n" +
+            "A quantidade de ingressos vendidos é: " + ingressosVendidos + "\n" +
+            "E o lucro máximo esperado é de: R$" + lucro.toFixed(2)
+        )
+        preco = preco - 0.5
+        ingressosVendidos = ingressosVendidos + 26
+        lucro =(ingressosVendidos * preco) - despesas
+    }
+
 }
