ab710c5c5b251ed291e3e0efdbb48fd150ee5b12
VINICIUS MANFRIN ZAMARA <26458@unifacef.edu.br>
2025-05-14 22:09:37 -0300

    Correção e melhoria do exercício 2

--- EstruturaDeRepeticao/js/exercicio2.js
+++ EstruturaDeRepeticao/js/exercicio2.js
@@ -1,18 +1,40 @@
 function exercicio2(){
     let preco = 5
     let ingressosVendidos = 120
-    let despesas = 200
+    const despesas = 200
     let lucro =(ingressosVendidos * preco) - despesas
-    let contagem = 0
+    let aux = ""
 
-    while (preco > 1){
-        alert("Quando o preço do ingresso é: R$" + preco.toFixed(2) + "\n" +
-            "A quantidade de ingressos vendidos é: " + ingressosVendidos + "\n" +
-            "E o lucro máximo esperado é de: R$" + lucro.toFixed(2)
+    //Variavéis para calculo de maior lucro
+    let maiorLucro = 0
+    let precoMaiorLucro = 0
+    let ingressosVendidosMaiorLucro = 0
+
+    while (preco >= 1){
+        aux = aux + (
+            "Preço do ingresso: R$" + preco.toFixed(2) + " - " +
+            "Quantidade de ingressos vendidos: " + ingressosVendidos + " - " +
+            "Lucro máximo esperado: R$" + lucro.toFixed(2) + "\n"
         )
+        if (lucro > maiorLucro){
+            maiorLucro = lucro
+            precoMaiorLucro = preco
+            ingressosVendidosMaiorLucro = ingressosVendidos
+        }
         preco = preco - 0.5
         ingressosVendidos = ingressosVendidos + 26
-        lucro =(ingressosVendidos * preco) - despesas
+        lucro = (ingressosVendidos * preco) - despesas
     }
 
+    alert(aux)
+    alert(`O preço do ingresso que deu o maior lucro foi ${precoMaiorLucro}, vendendo ${ingressosVendidosMaiorLucro} ingressos e rendendo R$${maiorLucro}`)
+
 }
+/*
+        aux = aux + (
+            "Quando o preço do ingresso é: R$" + preco.toFixed(2) + "\n" +
+            "A quantidade de ingressos vendidos é: " + ingressosVendidos + "\n" +
+            "E o lucro máximo esperado é de: R$" + lucro.toFixed(2) + "\n" + 
+            "//////////" + "\n"
+        )
+*/
